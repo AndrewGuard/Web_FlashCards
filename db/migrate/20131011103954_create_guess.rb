f396725b0a9214313f1503e9012d7d11c8f9f4b2
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 10:54:48 -0500

    guesses migration

--- db/migrate/20131011103954_create_guess.rb
+++ db/migrate/20131011103954_create_guess.rb
@@ -1,4 +1,10 @@
 class CreateGuess < ActiveRecord::Migration
   def change
+    create_table :guesses do |t|
+      t.integer :round_id
+      t.string :result
+
+      t.timestamps
+    end
   end
 end
