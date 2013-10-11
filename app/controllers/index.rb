b99781ae3f8e49ea7366e53603bd755371d60db6
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 14:07:33 -0500

    index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,22 +7,35 @@ get '/deck/:id' do
   erb :deck
 end
 
+
+#--------login---------#
+
+
 get '/card/:id' do
   # @card = card.find(params[:id])
   erb :card
 end
 
+#--------login---------#
+
 get '/login' do
   erb :login
 end
 
+#--------rounds---------#
+
 get '/round' do
-  # @round = round.find(params[:id])
   erb :round
 end
 
-# post '/round' do
-#   @current_round = Round.create()
 
-#   @current_round
-# end
+
+post '/createround/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  
+  @current_round = Round.create()
+
+  @current_round
+end
+
+
