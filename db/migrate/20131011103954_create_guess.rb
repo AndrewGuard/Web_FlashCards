aae23b3fb21670b9fda30bf92a718fb3f8a6f262
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 16:14:13 -0500

    added bcrypt stuff

--- db/migrate/20131011103954_create_guess.rb
+++ db/migrate/20131011103954_create_guess.rb
@@ -2,6 +2,7 @@ class CreateGuess < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
       t.integer :round_id
+      t.integer :card_id
       t.string :result
 
       t.timestamps
