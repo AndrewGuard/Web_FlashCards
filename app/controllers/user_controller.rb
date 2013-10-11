c8774c3d5627b2606bb806c6794c2bcd7ae1c22a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 17:38:39 -0500

    create users and login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,3 +3,31 @@
 get '/login' do
   erb :login
 end
+
+post '/authuser' do
+  if current_user
+    redirect to '/' 
+  else
+    @user = User.find_by_user_name(params[:user_name])
+
+    if @user.authenticate(params[:password])
+      session[:id] = @user.id
+      erb :index
+    else
+      erb :login
+    end
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to ('/')
+end
+
+
+
+post '/createuser' do
+  new_user = User.create(params[:post])
+  session[:id] = new_user.id
+  erb :index
+end
