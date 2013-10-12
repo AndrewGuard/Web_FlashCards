6cd1d486a1ce27d34fad2a866bc6baef9f4fa286
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 19:57:05 -0500

    bitches on my deck

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,12 @@ get '/' do
   erb :index
 end
 
+# --------edit-----------#
+
+get '/edit' do
+  erb :edit
+end
+
 #--------deck---------#
 
 get '/deck/:id' do
@@ -27,31 +33,37 @@ get '/createround/:deck_id' do
 end
 
 get '/round/:id' do
-
-  @counter = params[:counter] || 0
-  @current_round = Round.find(params[:id])
-    @deck ||= Deck.find(@current_round.deck_id)
-    unless @counter >= @deck.cards.length
-    @card = @deck.cards[@counter]
-    erb :round
-    end
+    round = Round.find(params[:id])
+    deck ||= Deck.find(round.deck_id)
+    card_array = deck.cards
+    
+    redirect to "/round/#{round.id}/#{card_array.sample.id}"
 end
 
-# --------edit-----------#
-
-get '/edit' do
-  erb :edit
+get "/round/:round_id/:card_id" do
+  @card = Card.find_by_id(params[:card_id])
+  @current_round = Round.find_by_id(params[:round_id])
+  erb :round
 end
 
 # --------guess-----------#
 
-post '/guess/:round_id/:card_id' do
+post "/guess/:round_id/:card_id" do
   #CREATE CORRECT METHOD FOR CARD
-  @guess = Guess.create(result: Card.find(params[:card_id].to_i).correct?(params[:answer]), 
-                        round_id: params[:round_id], 
-                        card_id: params[:card_id])
-  @counter = @guess.id
-  redirect to "/round/#{params[:round_id]}/?counter=#{@guess.id}"
+  new_guess = Guess.new(round_id: params[:round_id], card_id: params[:card_id])
+  @round_now = Round.find(params[:round_id])
+  @card_array = @round_now.deck.cards
+  @card = Card.find(params[:card_id])
+
+  if params[:answer] == @card.answer
+    new_guess.result = true
+    new_guess.save
+    redirect to "/round/#{@round_now.id}/#{@card_array.sample.id}"
+  else
+    new_guess.result = false
+    new_guess.save
+    redirect to "/"#round/{@round_now.id}/#{params[:card_id]}"
+  end
 end
 
 # --------resluts--------#
